b7e3d4a464fe8076807172b6e2019a9e76cfc5f7
Mert Mahmut Yiğit <117480842+mertmahmutyigit@users.noreply.github.com>
2024-08-12 15:07:58 +0300

    Fake Api
    Flutter ile fake api den veri çekme

---
+++ api/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:api/views/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FOOD recipe',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+        )
+
+
+      ),
+      home:  HomePage(),
+    );
+  }
+}
